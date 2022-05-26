4fd8a4709aa1efd9ed07e7a9698d2f2215016992
JamieDG <JamieDGhassibi@gmail.com>
2022-05-26 11:26:12 -0400

    Use list instead of dictionary
    Python's json library dumps data into a python dictionary, which only
    supports unique fields. XML, however, allows non-unique fields. I
    convert the dictionaries to lists to match XML's approach, so that if we
    need multiples of the same field, we can use them.

--- json2xml.py
+++ json2xml.py
@@ -1,34 +1,57 @@
+# Changes
+#   Use list structure instead of dictionary
+
 import json
-import xml
+from xml.etree.ElementTree import Element, tostring
+from dicttoxml import dicttoxml
 
 jsonpath = r'sample-files/photo_4583705695.json'
 
 json2xml_fields = {
     'name': 'title',
-    'description': 'subject',
+    'description': 'description',
     'date_taken': 'date',
-    'geo': 'latitude, longitude, accuracy',
-    'tags': '????????????'
+    'tags': 'tags', # se filtered_data
     }
+
+default_fields = {
+    'type': 'Photographs',
+}
 
 def main():
     with open(jsonpath) as file:
-        data = json.load(file)
-    filtered_data = filter_fields(data, json2xml_fields)
-    examine(filtered_data)
+        json_data = json.load(file)
+    list_data = json2list(json_data, json2xml_fields)
+    examine(list_data)
+    #xml_data = dict_to_xml_CHANGE('TEST', list_data) # change to list2xml
+    print()
+    #print(tostring(xml_data))
+    #print(dicttoxml(filtered_data))
 
-def filter_fields(data, fields):
-    filtered_data = {}
+def json2list(data, fields):
+    filtered_data = []
     for k, v in data.items():
         if k == 'tags':
-            v = [tag['tag'] for tag in v]
+            v = ','.join(tag['tag'] for tag in v)
         if k in fields:
-            filtered_data[k] = v
+            k = 'dc:' + fields[k]
+            filtered_data.append((k, v))
     return filtered_data
 
-def examine(data):
-    for k, v in data.items():
-        print(k, v, sep=': ')
+def list2xml(tag, d):  # TODO read through code carefully
+    # From [insert website]
+    raise NotImplemented
+    elem = Element(tag)
+    for key, val in d.items():
+        # create an Element
+        # class object
+        child = Element(key)
+        child.text = str(val)
+        elem.append(child)
+    return elem
 
+def examine(data):
+    for k, v in data:
+        print(k, v, sep='  ')
 
 main()
