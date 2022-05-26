6ac2ca11cb5ee8a9836d63e202a38f228401ff8e
JamieDG <JamieDGhassibi@gmail.com>
2022-05-26 13:55:39 -0400

    Add default items to data list (partially done)

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 This script converts NECC archived photo data on Flickr JSON to Preservica XML.
 
 ## 2022-05-26: Notes from meeting with Michael Hearn regarding JSON to XML field conversion
+
 ```
-subject: blank
-description: description
-tags: CSV
-what are the name fields, look at actual data
-name: title
-type: Photographs (PLURAL)
-identi: filename
-coverage: lawrence or haverhill search
+- [x] subject: blank
+- [x] description: description
+- [x] tags: CSV
+- [ ] what are the name fields? look at actual data
+- [x] name: title
+- [x] type: Photographs (PLURAL)
+- [ ] identifier: filename
+- [ ] coverage: lawrence or haverhill search
 ```
--- json2xml.py
+++ json2xml.py
@@ -1,42 +1,66 @@
-# Changes
-#   Use list structure instead of dictionary
+# TODO Add comments
+# TODO Convert lists to XML
 
 import json
+from pprint import pprint
 from xml.etree.ElementTree import Element, tostring
 from dicttoxml import dicttoxml
 
-jsonpath = r'sample-files/photo_4583705695.json'
+json_path = r'sample-files/photo_4583705695.json'
+
+def main():
+    with open(json_path) as file:
+        json_data = list(json.load(file).items())
+    data = filter_fields(json_data, json2xml_fields)
+    pprint(data)
+    data = add_fields(data, json_path)
+    pprint(data)
+    #picture_xml = dict2xml_CHANGE('TEST', data) # change to list2xml
+    print()
+    #print(tostring(picture_xml))
+    #print(dicttoxml(filtered_data))
 
 json2xml_fields = {
     'name': 'title',
     'description': 'description',
     'date_taken': 'date',
-    'tags': 'tags', # se filtered_data
+    'tags': 'tags',
     }
 
-default_fields = {
-    'type': 'Photographs',
-}
-
-def main():
-    with open(jsonpath) as file:
-        json_data = json.load(file)
-    list_data = json2list(json_data, json2xml_fields)
-    examine(list_data)
-    #xml_data = dict_to_xml_CHANGE('TEST', list_data) # change to list2xml
-    print()
-    #print(tostring(xml_data))
-    #print(dicttoxml(filtered_data))
-
-def json2list(data, fields):
+def filter_fields(data, fields):
     filtered_data = []
-    for k, v in data.items():
+    for k, v in data:
         if k == 'tags':
             v = ','.join(tag['tag'] for tag in v)
         if k in fields:
             k = 'dc:' + fields[k]
             filtered_data.append((k, v))
     return filtered_data
+
+def add_fields(data, json_path):
+    # Use list as look up and keys{}, vals{} are unnecessary.
+    default_xml_fields = {
+        'subject': '',
+        'type': 'Photographs',
+        'identifier': (get_filename, json_path),
+    }
+    for k, v in default_xml_fields.items():
+        # Values are usually strings, but if a function is at index
+        # 0, then call the function using the parameters in the rest
+        # of the value.
+        try isinstance(v, str) or callable(v[0]):
+            pass
+        except TypeError:
+            raise Exception('Value in default field is not string or tuple beginning with function', k)
+        if callable(v)[0]:
+            v = v(*v[1:])
+        data.append((k, v))
+    # TODO Add fields to input data
+    # TODO Add functions as needed
+    return data
+
+def get_filename():
+    pass
 
 def list2xml(tag, d):  # TODO read through code carefully
     # From [insert website]
@@ -49,9 +73,5 @@ def list2xml(tag, d):  # TODO read through code carefully
         child.text = str(val)
         elem.append(child)
     return elem
-
-def examine(data):
-    for k, v in data:
-        print(k, v, sep='  ')
 
 main()
